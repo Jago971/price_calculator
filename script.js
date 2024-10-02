f8bf38ee7a79d7dc3b36e0f5e3ed4a159bf94012
Matt_Mannings <matt.mannings97@gmail.com>
2024-10-02 11:58:53 +0100

    cat approved some more

--- script.js
+++ script.js
@@ -1,57 +1,52 @@
-'use strict';
+"use strict";
 
 function getPageViews(value) {
-    if (value == 100) {
-        return '1M'
-    } else {
-        return `${value * 10}K`
-    }
+  if (value == 100) {
+    return "1M";
+  } else {
+    return `${value * 10}K`;
+  }
 }
 
 function getPrice(value) {
-    if (value == 100) {
-            return 36
-        
-    } else if (value < 5) {
-            return 8
-        
-    } else if (value < 10) {
-            return 12
-        
-    } else if (value < 50) {
-            return 16
-        
-    } else {
-            return 24
-
-    }
+  if (value < 5) {
+    return 8;
+  } else if (value < 10) {
+    return 12;
+  } else if (value < 50) {
+    return 16;
+  } else if (value < 100) {
+    return 24;
+  } else {
+    return 36;
+  }
 }
 
-function yearlyBilling (value) {
-    return value * 0.75 * 12
+function yearlyBilling(value) {
+  return value * 0.75 * 12;
 }
 
-const slider = document.getElementById("slider")
-const pageViews = document.querySelector(".pageviews>p:first-of-type")
-const price = document.querySelector(".price>h1")
-const monthYear = document.querySelector(".price>p")
-const yearly = document.getElementById("yearly")
+const slider = document.getElementById("slider");
+const pageViews = document.querySelector(".pageviews>p:first-of-type");
+const price = document.querySelector(".price>h1");
+const monthYear = document.querySelector(".price>p");
+const yearly = document.getElementById("yearly");
 
-pageViews.innerHTML = getPageViews(slider.value)
-price.innerHTML = `$${getPrice(slider.value)}.00`
+pageViews.innerHTML = getPageViews(slider.value);
+price.innerHTML = `$${getPrice(slider.value)}.00`;
 
 slider.addEventListener("change", () => {
-    pageViews.innerHTML = getPageViews(slider.value)
-    price.innerHTML = `$${getPrice(slider.value)}.00`
-})
+  pageViews.innerHTML = getPageViews(slider.value);
+  price.innerHTML = `$${getPrice(slider.value)}.00`;
+});
 
 yearly.addEventListener("click", () => {
-    if(yearly.checked == true) {
-        let priceFinalv2 = yearlyBilling(getPrice(slider.value))
-        price.innerHTML = `$${priceFinalv2}.00`
-        monthYear.innerHTML = '&nbsp/ yearly';
-    } else {
-        price.innerHTML = `$${getPrice(slider.value)}.00`
-        monthYear.innerHTML = '&nbsp/ monthly';
-    }
-})
+  if (yearly.checked == true) {
+    let priceFinalv2 = yearlyBilling(getPrice(slider.value));
+    price.innerHTML = `$${priceFinalv2}.00`;
+    monthYear.innerHTML = "&nbsp/ yearly";
+  } else {
+    price.innerHTML = `$${getPrice(slider.value)}.00`;
+    monthYear.innerHTML = "&nbsp/ monthly";
+  }
+});
