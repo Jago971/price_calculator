de7a5f77858d4d34168879449d1162e994171780
Matt_Mannings <matt.mannings97@gmail.com>
2024-10-02 11:56:21 +0100

    tidier - cat approved

--- script.js
+++ script.js
@@ -1,21 +1,4 @@
-const slider = document.getElementById("slider")
-const pageViews = document.querySelector(".pageviews>p:first-of-type")
-const price = document.querySelector(".price>h1")
-const monthYear = document.querySelector(".price>p")
-const yearly = document.getElementById("yearly")
-
-let isYearly = false;
-
-pageViews.innerHTML = getPageViews(slider.value)
-price.innerHTML = `$${getPrice(slider.value)}.00`
-
-function monthOrYear(isYearly) {
-    if(isYearly) {
-        return 'yearly'
-    } else {
-        return 'monthly'
-    }
-}
+'use strict';
 
 function getPageViews(value) {
     if (value == 100) {
@@ -27,35 +10,20 @@ function getPageViews(value) {
 
 function getPrice(value) {
     if (value == 100) {
-        if(isYearly) {
-            return yearlyBilling(36)
-        } else {
             return 36
-        }
+        
     } else if (value < 5) {
-        if(isYearly) {
-            return yearlyBilling(8)
-        } else {
             return 8
-        }
+        
     } else if (value < 10) {
-        if(isYearly) {
-            return yearlyBilling(12)
-        } else {
             return 12
-        }
+        
     } else if (value < 50) {
-        if(isYearly) {
-            return yearlyBilling(16)
-        } else {
             return 16
-        }
+        
     } else {
-        if(isYearly) {
-            return yearlyBilling(24)
-        } else {
             return 24
-        }
+
     }
 }
 
@@ -63,20 +31,27 @@ function yearlyBilling (value) {
     return value * 0.75 * 12
 }
 
+const slider = document.getElementById("slider")
+const pageViews = document.querySelector(".pageviews>p:first-of-type")
+const price = document.querySelector(".price>h1")
+const monthYear = document.querySelector(".price>p")
+const yearly = document.getElementById("yearly")
+
+pageViews.innerHTML = getPageViews(slider.value)
+price.innerHTML = `$${getPrice(slider.value)}.00`
+
 slider.addEventListener("change", () => {
     pageViews.innerHTML = getPageViews(slider.value)
     price.innerHTML = `$${getPrice(slider.value)}.00`
-    monthYear.innerHTML = `&nbsp/ ${monthOrYear(isYearly)}`
 })
 
 yearly.addEventListener("click", () => {
     if(yearly.checked == true) {
-        isYearly = true
-        price.innerHTML = `$${getPrice(slider.value)}.00`
-        monthYear.innerHTML = `&nbsp/ ${monthOrYear(isYearly)}`
+        let priceFinalv2 = yearlyBilling(getPrice(slider.value))
+        price.innerHTML = `$${priceFinalv2}.00`
+        monthYear.innerHTML = '&nbsp/ yearly';
     } else {
-        isYearly = false
         price.innerHTML = `$${getPrice(slider.value)}.00`
-        monthYear.innerHTML = `&nbsp/ ${monthOrYear(isYearly)}`
+        monthYear.innerHTML = '&nbsp/ monthly';
     }
 })
